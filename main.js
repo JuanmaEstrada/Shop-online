f0d918039263aa9f84ce0729222818b58b9cba81
Usuario <juanmaestradarodriguez@gmail.com>
2018-04-16 15:57:17 +0200

    firs commit

---
+++ main.js
@@ -0,0 +1,22 @@
+Desarrolla una tienda!
+
+Una interfaz de una sola vista
+Tiene que tener 5-10 productos. Escribe un JSON (local) para esos productos
+(Nombre, foto, descripción precio!)
+En la interfaz tiene que haber un carrito de la compra (te acuerdas del que hiciste?? o no)
+
+Cuando alguien seleccione en el botón de añadir en un producto, 
+debe aparecer reflejado en el carrito. 
+
+El carrito debe tener botón de añadir y quitar cantidades
+
+El carrito debe darme el precio de cada cosa, así como multiplicado por sus unidades
+y finalmente el precio total.
+
+
+
+Y bonus:
+
+Ofertas: IVA; Añadir nuevos productos según categoría a la interfaz...
+
+Buscador
