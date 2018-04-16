b8d6d89d0b68277d474ad0ad2e76ddf179e6c648
Usuario <juanmaestradarodriguez@gmail.com>
2018-04-16 17:46:14 +0200

    thirds comment

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var Product = function(photo,name, price,description){
+var Product = function(photo,name,price,description){
 	this.photo=photo;
 	this.name=name;
 	this.price=price;
@@ -18,43 +18,21 @@ var product3 = new Product('https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/2016
 var html="";
 productShop={product0, product1, product2, product3};
 for(k in productShop){
-	html+=  <div class="row">
-			<div class="col-md-9 mt-4 mb-4 ">
-				<div class="row">
-					<div class="col-md-3"> 
-						<img src="https://www.lahiguera.net/musicalia/artistas/la_pegatina/disco/8934/portada-m.jpg" class="img-fluid" alt="">
-						<h4> La pegatina</h4>
-						<p>Ahora o nunca.</p>
-						<h5>10€</h5>
-						<button class="btn btn-primary"> Comprar</button>
-						 </div>
-					<div class="col-md-3 img-fluid">
-					<img src="https://pre00.deviantart.net/dd35/th/pre/i/2013/351/2/b/ac_dc_thunderstruck_album_reimagined_by_jerle73-d6yb5kr.jpg" class="img-fluid" alt=""> 
-						<h4> AC/DC</h4>
-						<p> Thunderstruk </p>
-						<h5>18€</h5>
-						<button class="btn btn-primary"> Comprar</button>
-					</div>
-					<div class="col-md-3"> 
-						<img src="https://gcdn.emol.cl/discos/files/2016/11/metallica-hardwired-to-self-destruct-768x768.jpg" class="img-fluid" alt="">
-						<h4> Metallica</h4>
-						<p>Hardwire, to self-destruct</p>
-						<h5>17€</h5>
-						<button class="btn btn-primary"> Comprar</button>
-					</div>
-					<div class="col-md-3"> 
-						<img src="https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201607/05/00105110274692____2__600x600.jpg" alt="" class="img-fluid">
-						<h4> Extremoduro</h4>
-						<p>Iros todos a ..</p>
-						<h5>20€</h5>
-						<button class="btn btn-primary"> Comprar</button>
-					</div>
-
-				</div>				
-			</div>
+	html+=  '<section class=" top container ">'+
+			'<div class="row">'+
+			'<div class="col-md-9 mt-4 mb-4 ">'+
+				'<div class="row">'+
+					'<div class="col-md-3">'+
+						'<img class ="photo1 img-fluid" src='+productShop[k].photo+'alt="Imagen descriptiva del producto">'+
+						'<h4>'+productShop[k].name+'</h4>'+
+						'<p>'+productShop[k].description+'</p>'+
+						'<h5>'+productShop[k].price+'</h5>'+
+						'<button class="btn btn-primary"> Comprar</button>'+
+						 '</div>'
+			
 }
 
-
+$('.top').append(html)
 
 })
 
